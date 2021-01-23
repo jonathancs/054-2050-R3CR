b28a01f98878d825008254690703a5f74d784397
jonathan casagrande <casagrandesoaresj@gmail.com>
2021-01-23 16:13:59 -0300

    progress achieved. To Do list yet to be done

--- configs/3CVsToUpload.js
+++ configs/3CVsToUpload.js
@@ -1,9 +1,6 @@
 let listOfCVsToUpload = [
-    //'C:/Users/Jonathan Casagrande/Downloads/cvs/toBeUploaded/ (1).pdf',
+    'C:/Users/Jonathan Casagrande/Downloads/cvs/toBeUploaded/ (1).pdf',
 
-    'D:/Usuarios/Jonnyboy/Downloads/Profile (1).pdf',
-    'D:/Usuarios/Jonnyboy/Downloads/Profile (2).pdf'
-    
 
 
 ]
